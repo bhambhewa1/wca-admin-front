f5ffc7ec12c99ac91c7aa72bbe5dff2e050cc3a2
unknown <ishika@orientaloutsourcing.com>
2023-03-16 18:08:19 +0530

    login

--- src/redux/services/login/index.js
+++ src/redux/services/login/index.js
@@ -1,10 +1,9 @@
 import {Api} from '../../../config/request'
 
   export const requestLogin = async (data) => {
-    return Api.PostRequest('/api/auth/login', data);
+    return Api.apiRequest('/api/auth/login', data);
   };
 const LoginAPI = {
     requestLogin,
-    
   };
   export default LoginAPI
