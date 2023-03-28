e7527e53286888518cdcb3162277f909c90d0454
vibhupuri3005 <vibhu.puri@orientaloutsourcing.com>
2023-03-28 10:55:16 +0530

    upload document

--- src/const/Style.js
+++ src/const/Style.js
@@ -11,4 +11,11 @@ export const Style = {
     },
     tableCell: { width: "100px", fontSize: "13px", color: "#707070", fontWeight: "600" },
   },
+  button: {
+    textTransform: "none",
+    borderRadius: "5px",
+    pl:4,pr:4,
+    bgcolor: "#F15F23",
+    color: "white",
+  }
 };
