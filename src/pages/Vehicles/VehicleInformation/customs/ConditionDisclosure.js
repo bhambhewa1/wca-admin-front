610629fd5d4de15dfbdf68a6ebb58f28e4831dee
unknown <harsh@orientaloutsourcing.com>
2023-04-03 12:55:05 +0530

    vehicle

--- src/pages/Vehicles/VehicleInformation/customs/ConditionDisclosure.js
+++ src/pages/Vehicles/VehicleInformation/customs/ConditionDisclosure.js
@@ -1,7 +1,16 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import React from "react";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Light from "../../../../assests/Light@2x.png";
+import Body from "../../../../assests/Body@2x.png";
+import Mechanical from "../../../../assests/Mechanical@2x.png";
+import Other from "../../../../assests/Other@2x.png";
+import Glass from "../../../../assests/Glass@2x.png";
+import Interior from "../../../../assests/Interior@2x.png";
+import Tyre from "../../../../assests/Wheel@2x.png";
+import Market from "../../../../assests/After Market@2x.png";
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
@@ -10,7 +19,26 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
   boxShadow: "none",
 }));
-const data = ["Lights", "Mechanical", "Other", "Body", "Glass", "Interior", "Tyre/Wheel", "After Market"];
+const data = [
+  { text: "Lights", icon: Light },
+  { text: "Mechanical", icon: Mechanical },
+  { text: "Other", icon: Other },
+  { text: "Body", icon: Body },
+  { text: "Glass", icon: Glass },
+  { text: "Interior", icon: Interior },
+  { text: "Tyre/Wheel", icon: Tyre },
+  { text: "After Market", icon: Market },
+];
+const data1 = [
+  { text: "Abs", icon: Light },
+  { text: "Battery", icon: Mechanical },
+  { text: "Brake", icon: Other },
+  { text: "Engine", icon: Body },
+  { text: "Traction", icon: Glass },
+  { text: "Airbag/srs", icon: Interior },
+  { text: "Tpms", icon: Tyre },
+  { text: "Hybrid battery", icon: Market },
+];
 const ConditionDisclosure = () => {
   return (
     <>
@@ -21,22 +49,58 @@ const ConditionDisclosure = () => {
           <Grid
             flex={"1 1 auto"}
             sx={{
-              border: "3px solid #ECECEC",
+              border: "2px solid #ECECEC",
               mt: "10px",
               ml: "10px",
               mr: "10px",
               borderRadius: "10px",
+              textAlign: "center",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              p: "20px",
+            }}
+            item>
+            <img alt="icon" style={{ width: "30px" }} src={item.icon} />
+
+            <Item
+              sx={{
+                fontSize: "14px",
+                color: "#000",
+                p: "0px",
+                boxShadow: "none",
+              }}>
+              {item.text}
+              <Typography sx={{ color: "#007A4D" }}>$0</Typography>
+            </Item>
+          </Grid>
+        ))}
+      </Grid>
+      <Divider sx={{ width: "98%", ml: "10px", border: "1px solid #ECECEC" }} />
+      <Grid
+        sx={{ ml: "10px", mr: "10px", width: "50%", flexWrap: "wrap", mt: "10px", mb: "10px" }}
+        container
+        rowGap={"10px"}
+        columnGap={"10px"}>
+        {data1.map((item, index) => (
+          <Grid
+            flex={"1 1 auto"}
+            sx={{
+              border: "2px solid #ECECEC",
+              borderRadius: "5px",
+              minWidth: "15%",
+              maxWidth: "18%",
             }}
             item>
             <Item
               sx={{
-                p: "20px",
-                fontSize: "14px",
+                fontSize: "12px",
                 color: "#000",
+                p: "5px ",
                 boxShadow: "none",
               }}>
-              {item}
-              <Typography sx={{ color: "#007A4D" }}>$0</Typography>
+              {item.text}
             </Item>
           </Grid>
         ))}
