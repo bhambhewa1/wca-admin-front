a18f8532fcb601c243817b9f79e39429c014ca89
Ashish <ashish1001ashu@gmail.com>
2023-10-04 07:41:05 +0530

    second commit

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducer";
-// import thunkMiddleware from 'redux-thunk'
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
